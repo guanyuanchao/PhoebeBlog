f46ee2a6e4d43ba358bfbb7ed6a32fd845a934dc
Alex <guanyuanchao@funguide.com.cn>
2015-10-31 21:38:19 +0800

    删除多余测试文件

--- config/application.rb
+++ config/application.rb
@@ -32,8 +32,8 @@ module PhoebeBlog
       :port => 25,
       :domain => "163.com",
       :authenticcation => :login,
-      :user_name => "XXXXXXXXXXXX@163.com",
-      :password => "XXXXXXXXXXXXX"
+      :user_name => "gych373027161@163.com",
+      :password => "zhangyue@qq.com"
      }
   end
 end
