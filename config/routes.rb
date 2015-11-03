34bf215afa19cfd2ef848ae450abad1408b08bc7
Alex <guanyuanchao@funguide.com.cn>
2015-11-04 00:57:34 +0800

    定制devise，增加用户名

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :users
-  
-  # The priority is based upon order of creation: first created -> highest priority.
+  #devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions",
+                                    confirmations: "users/confirmations",
+                                    passwords: "users/passwords",
+                                    registrations: "users/registrations",
+                                    unlocks: "users/unlocks"}
+  # # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
