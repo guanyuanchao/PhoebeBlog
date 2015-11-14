b936e80e6c89778f6b9f7da8f92bfdbc85626e29
Alex <guanyuanchao@funguide.com.cn>
2015-11-14 17:46:29 +0800

    完善文章模块

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  ARTICLE_TYPE = {
+  $ARTICLE_TYPE = {
   	I18n.t("emotion") => 1,
   	I18n.t("travel") => 2,
   	I18n.t("life") => 3
   }
-  ARTICLE_TYPE_REVERT = {
+  $ARTICLE_TYPE_REVERT = {
   	1 => I18n.t("emotion"),
   	2 => I18n.t("travel"),
   	3 => I18n.t("life")
