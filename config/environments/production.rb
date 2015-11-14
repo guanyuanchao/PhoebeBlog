b30f05044f9b0985614da2098949930133202c5b
Alex <guanyuanchao@funguide.com.cn>
2015-11-14 19:52:19 +0800

    修改邮件服务的host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options  = { host: '52.192.166.98',  port: 80  }
+  config.action_mailer.default_url_options  = { host: 'www.wanwanblog.com'}
 end
